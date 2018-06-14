f97f114b9b507408213d4a58ee3f720c42338fe6
DanielPHobbs <Daniel.hobbs@dtekitinnovations.com>
2018-06-14 15:13:39 +0100

    edit

---
+++ test scripts/Test Import module.ps1
@@ -0,0 +1,34 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.152
+	 Created on:   	14/06/2018 14:56
+	 Created by:   	danie
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+$logMessages=""
+
+if (-not (Get-Module -Name 'OperationsManager'))
+{
+	if (Get-Module -ListAvailable | Where-Object { $_.name -eq 'OperationsManager' })
+	{
+		$LogMessages += "The Operations Manager Module was not found...importing the Operations Manager Module" + $Global:NewLine
+		load_module OperationsManager
+	}
+	else
+	{
+		$LogMessages += "The Operations Manager Module is not available on this system --- console will not function" + $Global:NewLine
+		$Status ="failed"
+	}
+}
+else
+{
+	$LogMessages += "The Operations Manager Module is loaded" + $Global:NewLine
+}
+$logmessages
+$Status
